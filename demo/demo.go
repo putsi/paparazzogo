b597d62f0597d0bd8014979c9df1ca03d58e5ed4
Jarmo Puttonen <f0899@student.jamk.fi>
2014-04-27 00:33:51 +0300

    Fix demo.go bugs

--- demo/demo.go
+++ demo/demo.go
@@ -10,15 +10,14 @@ func main() {
 	// Local server settings
 	imgPath := "/img.jpg"
 	addr := ":8080"
-	
+
 	// MJPEG-stream settings
 	user := ""
 	pass := ""
 	timeout := 5 * time.Second
 	mjpegStream := "http://westunioncam.studentaffairs.duke.edu/mjpg/video.mjpg"
-	
-	
-	mp := mjpegproxy.NewMjpegproxy()
+
+	mp := paparazzogo.NewMjpegproxy()
 
 	mp.StartCrawling(mjpegStream, user, pass, timeout)
 	mp.Serve(imgPath, addr)
